d509528f97b91cf83e2140b457c99db345429ee7
Mark Proctor <mdproctor@gmail.com>
2026-04-28 00:31:34 +0100

    fix(spi): MessageTypeViolationException extends IllegalStateException for WrapBusinessError coverage
    Refs #122

--- runtime/src/main/java/io/quarkiverse/qhorus/runtime/message/MessageTypeViolationException.java
+++ runtime/src/main/java/io/quarkiverse/qhorus/runtime/message/MessageTypeViolationException.java
@@ -1,6 +1,6 @@
 package io.quarkiverse.qhorus.runtime.message;
 
-public class MessageTypeViolationException extends RuntimeException {
+public class MessageTypeViolationException extends IllegalStateException {
 
     public MessageTypeViolationException(String channel, MessageType attempted, String allowed) {
         super("Channel '" + channel + "' does not permit " + attempted + ". Allowed: " + allowed);
